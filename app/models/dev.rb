c38f962101f2785ced3b17f4e4ee014974ac53cc
Allen Mordukhaev <allenmor93@gmail.com>
2022-09-07 11:22:25 -0400

    done

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,14 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        Freebie.where(item_name: item_name) == [] ? false : true
+    end
+    
+    def give_away(dev, freebie)
+        if self == freebie.dev
+            freebie.dev = self
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+require 'faker'
+
+Company.destroy_all
+Dev.destroy_all
+Freebie.destroy_all
+
 puts "Creating companies..."
 Company.create(name: "Google", founding_year: 1998)
 Company.create(name: "Facebook", founding_year: 2004)
@@ -12,10 +18,13 @@ Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
-# ***************************************************************
-# * TODO: create freebies! Remember, a freebie belongs to a dev *
-# * and a freebie belongs to a company.                         *
-# ***************************************************************
-# Create freebies Here
+10.times do |t|
+    Freebie.create(
+        item_name: Faker::Food.fruits,
+        value: Faker::Number.number(digits: 3),
+        dev_id: Dev.all.sample.id,
+        company_id: Company.all.sample.id
+    )
+end
 
 puts "Seeding done!"
